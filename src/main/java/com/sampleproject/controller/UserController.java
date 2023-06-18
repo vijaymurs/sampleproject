9f4a98e4fcbf3733437ce01208f671d99c6a63e4
Vijay <vijayssait@gmail.com>
2023-06-18 22:19:05 +0530

    test

--- src/main/java/com/sampleproject/controller/UserController.java
+++ src/main/java/com/sampleproject/controller/UserController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sampleproject.dto.UserRequest;
-import com.sampleproject.entity.UserEntity;
 import com.sampleproject.service.UserService;
 
 /**
